90e3a14f97ce13ca249dd8eed5c4369ae4e92a36
Zombori Tamas <ztamas2000@gmail.com>
2023-01-23 12:01:38 +0100

    Flask service now predicts from question and context returned by elasticsearch

--- flask_service.py
+++ flask_service.py
@@ -1,13 +1,20 @@
 from flask import Flask
 from elasticsearch import Elasticsearch
-
+from transformers import pipeline, AutoTokenizer, AutoModelForQuestionAnswering
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
+tokenizer = AutoTokenizer.from_pretrained("ZTamas/hubert-qa-milqa")
+model = AutoModelForQuestionAnswering.from_pretrained("ZTamas/hubert-qa-milqa")
+qa_pipeline = pipeline(
+    "question-answering",
+    model=model,
+    tokenizer=tokenizer
+)
 
 
 @app.route('/<query>')
-def hello(query):
+def predict_from_question(query):
     body = {
         "query": {
             "match": {
@@ -22,20 +29,27 @@ def hello(query):
     }
 
     es = Elasticsearch(
-        "http://rgai3.inf.u-szeged.hu:3427/",
-        basic_auth=("elastic", "V7uek_ey6EdQbGBz_XHX"),
+        "http://rgai3.inf.u-szeged.hu:3427/",  # localhostra átírni tesztelésre
+        http_auth=("elastic", "V7uek_ey6EdQbGBz_XHX"),
         verify_certs=False
     )
 
     s = es.search(index='milqa', body=body)
 
-    result_list = list()
+    # The query only returns the text before the question mark, so we add it here.
+    question = query + '?'
+    # We use the highest ranked document by the elasticsearch.
+    context = s['hits']['hits'][0]["_source"]["document"]
+    prediction = qa_pipeline({
+        'context': context,
+        'question': question
+    })
 
-    for hit in s['hits']['hits']:
-        result_dict = dict()
-        # print("context: ", hit["_source"], "\n", "highlight: ", hit["highlight"])
-        query_result = " ------- ".join(hit["highlight"]["document"])
-        result_dict[hit["_source"]["document"]] = query_result
-        result_list.append(result_dict)
+    return_value = {"context": context,
+                    "question": question,
+                    "answer": prediction['answer'],
+                    "start": prediction['start'],
+                    "end": prediction['end'],
+                    "score": prediction['score']}
 
-    return result_list
+    return return_value
