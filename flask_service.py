9de3025b61290e6eb02cc2b3375b0c4a040c73d4
SzaboGergo01 <gergo.mizo@gmail.com>
2023-02-22 12:56:19 +0100

    feat(flask): added multiple model chooser

--- flask_service.py
+++ flask_service.py
@@ -10,98 +10,37 @@ app.config['JSON_AS_ASCII'] = False
 with open('config.json', 'r') as config:
     config_variables = json.load(config)
 
-# official huBERT (short-nonimpossible)
-question_answering_hubert = config_variables["hubert_sni_qa_pipeline"][0]["pipeline"]
-tokenizer_hubert = AutoTokenizer.from_pretrained(config_variables["hubert_sni_qa_pipeline"][1]["tokenizer"])
-model_hubert = AutoModelForQuestionAnswering.from_pretrained(config_variables["hubert_sni_qa_pipeline"][2]["model"])
 
-qa_pipeline_hubert_sni = pipeline(question_answering_hubert,
-                                  model=model_hubert,
-                                  tokenizer=tokenizer_hubert)
-
-# huBERT-short-impossible
-question_answering_hubert_si = config_variables["hubert_si_qa_pipeline"][0]["pipeline"]
-tokenizer_hubert_si = config_variables["hubert_si_qa_pipeline"][1]["tokenizer"]
-model_hubert_si = config_variables["hubert_si_qa_pipeline"][2]["model"]
-device_hubert_si = int(config_variables["hubert_si_qa_pipeline"][3]["device"])
-handle_impossible_answer_hubert_si = bool(config_variables["hubert_si_qa_pipeline"][4]["handle_impossible_answer"])
-max_answer_len_hubert_si = int(config_variables["hubert_si_qa_pipeline"][5]["max_answer_len"])
-
-qa_pipeline_hubert_si = pipeline(question_answering_hubert_si,
-                                 tokenizer=tokenizer_hubert_si,
-                                 model=model_hubert_si,
-                                 device=device_hubert_si,
-                                 handle_impossible_answer=handle_impossible_answer_hubert_si,
-                                 max_answer_len=max_answer_len_hubert_si)
-
-# huBERT-long-impossible
-question_answering_hubert_li = config_variables["hubert_li_qa_pipeline"][0]["pipeline"]
-tokenizer_hubert_li = config_variables["hubert_li_qa_pipeline"][1]["tokenizer"]
-model_hubert_li = config_variables["hubert_li_qa_pipeline"][2]["model"]
-device_hubert_li = int(config_variables["hubert_li_qa_pipeline"][3]["device"])
-handle_impossible_answer_hubert_li = bool(config_variables["hubert_li_qa_pipeline"][4]["handle_impossible_answer"])
-max_answer_len_hubert_li = int(config_variables["hubert_li_qa_pipeline"][5]["max_answer_len"])
-
-qa_pipeline_hubert_li = pipeline(question_answering_hubert_li,
-                                 tokenizer=tokenizer_hubert_li,
-                                 model=model_hubert_li,
-                                 device=device_hubert_li,
-                                 handle_impossible_answer=handle_impossible_answer_hubert_li,
-                                 max_answer_len=max_answer_len_hubert_li)
-
-# RoBERTa-short-impossible
-question_answering_roberta_si = config_variables["roberta_si_qa_pipeline"][0]["pipeline"]
-tokenizer_roberta_si = config_variables["roberta_si_qa_pipeline"][1]["tokenizer"]
-model_roberta_si = config_variables["roberta_si_qa_pipeline"][2]["model"]
-device_roberta_si = int(config_variables["roberta_si_qa_pipeline"][3]["device"])
-handle_impossible_answer_roberta_si = bool(config_variables["roberta_si_qa_pipeline"][4]["handle_impossible_answer"])
-max_answer_len_roberta_si = int(config_variables["roberta_si_qa_pipeline"][5]["max_answer_len"])
-
-qa_pipeline_roberta_si = pipeline(question_answering_roberta_si,
-                                  tokenizer=tokenizer_roberta_si,
-                                  model=model_roberta_si,
-                                  device=device_roberta_si,
-                                  handle_impossible_answer=handle_impossible_answer_roberta_si,
-                                  max_answer_len=max_answer_len_roberta_si)
-
-# RoBERTa-long-impossible
-question_answering_roberta_li = config_variables["roberta_li_qa_pipeline"][0]["pipeline"]
-tokenizer_roberta_li = config_variables["roberta_li_qa_pipeline"][1]["tokenizer"]
-model_roberta_li = config_variables["roberta_li_qa_pipeline"][2]["model"]
-device_roberta_li = int(config_variables["roberta-li_qa_pipeline"][3]["device"])
-handle_impossible_answer_roberta_li = bool(config_variables["roberta_li_qa_pipeline"][4]["handle_impossible_answer"])
-max_answer_len_roberta_li = int(config_variables["roberta_li_qa_pipeline"][5]["max_answer_len"])
-
-qa_pipeline_roberta_li = pipeline(question_answering_roberta_li,
-                                  tokenizer=tokenizer_roberta_li,
-                                  model=model_roberta_li,
-                                  device=device_roberta_li,
-                                  handle_impossible_answer=handle_impossible_answer_roberta_li,
-                                  max_answer_len=max_answer_len_roberta_li)
+all_models = dict()
+for model in config_variables["models"]:
+    all_models[model["visible_name"]] = pipeline(model["pipeline"],
+                                                 tokenizer=model["tokenizer"],
+                                                 model=model["model"],
+                                                 device=model["device"],
+                                                 handle_impossible_answer=model["handle_impossible_answer"],
+                                                 max_answer_len=model["max_answer_len"])
 
 
-# nlp_hu = spacy.load("hu_core_news_trf")
+nlp_hu = spacy.load("hu_core_news_trf")
 
 
 @app.route('/query/<query>')
 def predict_from_question(query, size, elastic, model_type):
-    # doc_q = nlp_hu(query)
-    # clean_tokens = list()
-    #
-    # for token in doc_q:
-    #     # print(token.text, token.pos_, token.dep_)
-    #     if token.pos_ not in ['DET', 'ADV', 'PRON', 'PUNCT']:
-    #         clean_tokens.append(token.lemma_)
-    #
-    # clean_question = " ".join(clean_tokens)
+    doc_q = nlp_hu(query)
+    clean_tokens = list()
 
-    prediction = dict()
+    for token in doc_q:
+        # print(token.text, token.pos_, token.dep_)
+        if token.pos_ not in ['DET', 'ADV', 'PRON', 'PUNCT']:
+            clean_tokens.append(token.lemma_)
+
+    clean_question = " ".join(clean_tokens)
 
     body = {
         "size": size,
         "query": {
             "match": {
-                "document": query
+                "document": clean_question
             }
         }
     }
@@ -126,31 +65,10 @@ def predict_from_question(query, size, elastic, model_type):
         official_context = context_raw["_source"]["official_document"]
         elastic_score = context_raw["_score"]
 
-        if model_type == "hubert-sni":
-            prediction = qa_pipeline_hubert_sni({
-                'context': official_context,
-                'question': official_question
-            })
-        elif model_type == "hubert_si":
-            prediction = qa_pipeline_hubert_si({
-                'context': official_context,
-                'question': official_question
-            })
-        elif model_type == "hubert_li":
-            prediction = qa_pipeline_hubert_li({
-                'context': official_context,
-                'question': official_question
-            })
-        elif model_type == "roberta_si":
-            prediction = qa_pipeline_roberta_si({
-                'context': official_context,
-                'question': official_question
-            })
-        elif model_type == "roberta_li":
-            prediction = qa_pipeline_roberta_li({
-                'context': official_context,
-                'question': official_question
-            })
+        prediction = all_models[model_type]({
+                    'context': official_context,
+                    'question': official_question
+                })
 
         return_value.append({"lemmatized_context": lemmatized_context,
                              "official_question": official_question,
@@ -180,11 +98,14 @@ def predict_from_question_gui():
                                query=query,
                                size=size,
                                elastic=elastic,
-                               model_type=model_type)
+                               model_type=model_type,
+                               config_variables=config_variables["models"])
 
     return render_template('index.html',
                            data=None,
-                           query=None)
+                           query=None,
+                           model_type=config_variables["models"][0]["visible_name"],
+                           config_variables=config_variables["models"])
 
 
 @app.route('/qa/api/', methods=['GET', 'POST'])
