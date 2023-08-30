319ef36836d727eebaab1da0c9d6c002dd6f7599
Peter Berkecz <berkecz.peter@gmail.com>
2023-08-30 15:03:25 +0200

    feat(frontend): :globe_with_meridians: added l10n

--- frontend/components/AnswerCard.vue
+++ frontend/components/AnswerCard.vue
@@ -6,7 +6,7 @@
         </div>
 
         <blockquote class="p-4 my-4 border-l-4 border-lime-600 bg-gray-200/80" v-if="answer.answer">
-            <p class="italic font-medium leading-relaxed text-gray-900">„{{ answer.answer }}”</p>
+            <p class="italic font-medium leading-relaxed text-gray-900">{{ $t('l10n.quoteStart') }}{{ answer.answer }}{{ $t('l10n.quoteEnd') }}</p>
         </blockquote>
 
         <div class="py-3 flex flex-row space-x-3 hover:brightness-125 transition duration-300 cursor-pointer"
@@ -32,7 +32,7 @@
         </div>
 
         <div v-if="open" class="text-slate-900 mb-3 flex flex-col space-y-2">
-            <h2 class="text-gray-500">Context</h2>
+            <h2 class="text-gray-500">{{ $t('app.context') }}</h2>
             <p class="text-justify">
                 {{ answer.relevant_context.slice(0, answer.start) }}
                 <span class="before:block before:absolute before:-inset-0.5 before:bg-slate-400 relative inline-block" v-if="answer.end != 0">
@@ -51,21 +51,21 @@
         </div>
 
         <div class="flex flex-col space-y-2" v-if="answer.metadata">
-            <h2 class="text-gray-500">Attachments</h2>
+            <h2 class="text-gray-500">{{ $t('app.source') }}</h2>
             <div class="flex flex-row space-x-2">
                 <Metadata v-for="metadata in answer.metadata" :metadata="metadata" />
             </div>
         </div>
         <div class="flex flex-row space-x-2 justify-end">
             <div class="hover:rotate-180 transition duration-300" v-if="config.debug"
-                :title="'Model score: ' + answer.model_score + '\nElastic score: ' + answer.elastic_score">
+                :title="$t('debug.modelScore') + ': ' + answer.model_score + '\n' + $t('debug.elasticScore') + ': ' + answer.elastic_score">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-6 h-6 text-lime-600">
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="M12 12.75c1.148 0 2.278.08 3.383.237 1.037.146 1.866.966 1.866 2.013 0 3.728-2.35 6.75-5.25 6.75S6.75 18.728 6.75 15c0-1.046.83-1.867 1.866-2.013A24.204 24.204 0 0112 12.75zm0 0c2.883 0 5.647.508 8.207 1.44a23.91 23.91 0 01-1.152 6.06M12 12.75c-2.883 0-5.647.508-8.208 1.44.125 2.104.52 4.136 1.153 6.06M12 12.75a2.25 2.25 0 002.248-2.354M12 12.75a2.25 2.25 0 01-2.248-2.354M12 8.25c.995 0 1.971-.08 2.922-.236.403-.066.74-.358.795-.762a3.778 3.778 0 00-.399-2.25M12 8.25c-.995 0-1.97-.08-2.922-.236-.402-.066-.74-.358-.795-.762a3.734 3.734 0 01.4-2.253M12 8.25a2.25 2.25 0 00-2.248 2.146M12 8.25a2.25 2.25 0 012.248 2.146M8.683 5a6.032 6.032 0 01-1.155-1.002c.07-.63.27-1.222.574-1.747m.581 2.749A3.75 3.75 0 0115.318 5m0 0c.427-.283.815-.62 1.155-.999a4.471 4.471 0 00-.575-1.752M4.921 6a24.048 24.048 0 00-.392 3.314c1.668.546 3.416.914 5.223 1.082M19.08 6c.205 1.08.337 2.187.392 3.314a23.882 23.882 0 01-5.223 1.082" />
                 </svg>
             </div>
-            <div class="cursor-pointer text-blue-600 hover:text-blue-800 transition duration-300" title="I like this answer"
+            <div class="cursor-pointer text-blue-600 hover:text-blue-800 transition duration-300" :title="$t('app.feedback.like')"
                 @click="sendFeedback">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-6 h-6">
@@ -74,7 +74,7 @@
                 </svg>
             </div>
             <div class="cursor-pointer text-orange-600 hover:text-orange-800 transition duration-300"
-                title="I don't like this answer" @click="feedback = !feedback">
+                :title="$t('app.feedback.dislike')" @click="feedback = !feedback">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round"
@@ -124,6 +124,8 @@ const toggleOpen = () => {
     feedback.value = !feedback.value
 }
 
+const { t } = useI18n()
+
 const sendFeedback = async () => {
     await useAsyncData('feedbackLike', () => {
         $fetch(`${config.apiUrl}/feedback/like`, 
@@ -134,9 +136,9 @@ const sendFeedback = async () => {
             }
         }
         ).then(() => {
-            useNuxtApp().$toast.success('Thank you for your feedback!')
+            useNuxtApp().$toast.success(t('app.feedback.toast.success'))
         }).catch(() => {
-            useNuxtApp().$toast.error('Something went wrong with sending your feedback. Please try again later.')
+            useNuxtApp().$toast.error(t('app.feedback.toast.error'))
         })
     })
 }
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -20,4 +20,10 @@ export default defineNuxtConfig({
       elasticTables: '',
     }
   },
+  modules: [
+    '@nuxtjs/i18n',
+  ],
+  i18n: {
+    vueI18n: './i18n.config.ts',
+  },
 });
