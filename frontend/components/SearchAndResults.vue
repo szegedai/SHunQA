6f17127ba523bd49b1186d7ee54a6c5d0e0e37f1
Peter Berkecz <berkecz.peter@gmail.com>
2023-10-12 15:29:15 +0200

    fix(frontend): fixed data loading on first load

--- frontend/components/SearchAndResults.vue
+++ frontend/components/SearchAndResults.vue
@@ -4,22 +4,22 @@
 
         <pre class="mb-2 italic">{{ $t('app.exampleQuestion') }}</pre>
         <SearchBar v-model:query="question" :pending="pending" :ask-question="refresh" :error="error" class="mb-4" />
-        <AskQuestion v-if="Object.keys(answer).length == 0 || (Object.keys(answer).length != 0 && pending)"
+        <AskQuestion v-if="(Object.keys(answer).length == 0 || (Object.keys(answer).length != 0 && pending)) && !error"
             :pending="pending" />
+        <Errors v-if="error" :error="error" />
         <AnswerCard :answer="answer" :question="question" :debug="answer.debug" v-if="!pending && answer" />
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const config = useRuntimeConfig().public
 const question = ref("")
+let answer = ref<any>({})
+let pending = ref(false)
+let error = ref<any>("")
+let refresh = ref()
 
-const {
-    data: answer,
-    pending,
-    error,
-    refresh,
-} = await useAsyncData("answers", () =>
+let data = await useLazyAsyncData("answers", () =>
     $fetch(`${config.apiUrl}/question`,
         {
             method: 'POST',
@@ -27,6 +27,14 @@ const {
                 "query": question.value,
             }
         }
-    )
+    ).then((res) => {
+        error.value = ""
+        answer.value = res
+    }).catch((err) => {
+        error.value = err
+        answer.value = {}
+    })
 )
+refresh.value = data.refresh
+pending = data.pending
 </script>
